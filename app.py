d4b8e141ed3fecbc319b2b6e947e86fceed4add1
Tanveer <49335574+w3tsa@users.noreply.github.com>
2023-05-12 02:43:34 +0000

    seeding the app

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 # pylint: disable=missing-function-docstring
 
+
 """
 Module docstring goes here.
 """
-
+from datetime import datetime
 from flask import Flask, render_template, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Hcnv6ARXyr2fqQazuYLFQ'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# pylint: disable=missing-class-docstring
+db.init_app(app)
+
+# pylint: disable=too-few-public-methods
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}'"
 
 posts = [
     {
@@ -25,6 +54,20 @@ posts = [
         'date_posted': 'May 11, 2023'
     }
 ]
+
+user_1 = User(username='some', email='t3@email.com', password='pass')
+user_2 = User(username='some other', email='s3@email.com', password='pass')
+
+with app.app_context():
+    db.create_all()
+    db.session.add(user_2)
+    db.session.commit()
+
+
+@app.route("/users")
+def user_list():
+    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    return render_template("users.html", users=users, title='Users')
 
 @app.route("/")
 @app.route("/home")
